579c83136d2f295742d00c545371b89fd7cd84bd
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2023-12-26 18:22:21 +0530

    Update Student.java

--- Design Patterns/Creational Design Patterns/Prototype Pattern/Student.java
+++ Design Patterns/Creational Design Patterns/Prototype Pattern/Student.java
@@ -1,1 +1,34 @@
+public class Student implements Prototype {
+    private String name;
+    private int age;
+    private String department;
+
+    public Student(String name, int age, String department) {
+        this.name = name;
+        this.age = age;
+        this.department = department;
+    }
+
+    // Getter and setter methods...
+
+    @Override
+    public Prototype clone() {
+        // Use default clone() method to create a shallow copy
+        try {
+            return (Student) super.clone();
+        } catch (CloneNotSupportedException e) {
+            // Handle the exception appropriately
+            return null;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", department='" + department + '\'' +
+                '}';
+    }
+}
 
