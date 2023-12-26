24d6ff2bd9492a1db2c68be8c0bc7b9e3f5d6298
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2023-12-26 18:24:08 +0530

    Update Main.java

--- Design Patterns/Creational Design Patterns/Prototype Pattern/Main.java
+++ Design Patterns/Creational Design Patterns/Prototype Pattern/Main.java
@@ -1,1 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        // Creating a prototype student
+        Student originalStudent = new Student("John Doe", 20, "Computer Science");
 
+        // Cloning the student to create a new instance
+        Student clonedStudent = (Student) originalStudent.clone();
+
+        // Modifying the cloned student
+        clonedStudent.setName("Jane Doe");
+        clonedStudent.setAge(22);
+
+        // Printing original and cloned students
+        System.out.println("Original Student: " + originalStudent);
+        System.out.println("Cloned Student: " + clonedStudent);
+    }
+}
