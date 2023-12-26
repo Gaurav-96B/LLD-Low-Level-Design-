87161b04580282246426326f7544d08d4fb76aeb
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2023-12-26 14:28:56 +0530

    Create EagerWayOfSingleton.java

---
+++ Design Patterns/Creational Design Patterns/Singleton Pattern/EagerWayOfSingleton.java
@@ -0,0 +1,11 @@
+public class Singleton {
+    private static final Singleton instance = new Singleton();
+    private Singleton() {
+        // Initialization code
+    }
+
+    // The public method to get the singleton instance
+    public static Singleton getInstance() {
+        return instance;
+    }
+}
