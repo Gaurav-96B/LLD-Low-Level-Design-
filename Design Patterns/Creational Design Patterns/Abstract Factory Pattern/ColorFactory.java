765baf2fec5a65d5598d5d64463efbb6af2782b9
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2023-12-26 16:31:02 +0530

    Create ColorFactory.java

---
+++ Design Patterns/Creational Design Patterns/Abstract Factory Pattern/ColorFactory.java
@@ -0,0 +1,26 @@
+public class ColorFactory extends AbstractFactory{	 
+   //getColor method returns object of input type color
+   @Override
+   Color getColor(String color) {   
+      if(color == null){
+         return null;
+      }		
+ 
+      if(color.equalsIgnoreCase("Red")){
+         return new Red();
+ 
+      }else if(color.equalsIgnoreCase("Green")){
+         return new Green();
+ 
+      }else if(color.equalsIgnoreCase("Blue")){
+         return new Blue();
+      }
+ 
+      return null;
+   }
+ 
+   @Override
+   public Shape getShape(String shapeType){
+      return null;
+   }
+}
