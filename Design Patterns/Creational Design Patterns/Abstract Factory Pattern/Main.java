7977a1a3dd8ea7e3c14a8c52d726de7e6bfe6e59
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2023-12-26 16:35:05 +0530

    Update Main.java

--- Design Patterns/Creational Design Patterns/Abstract Factory Pattern/Main.java
+++ Design Patterns/Creational Design Patterns/Abstract Factory Pattern/Main.java
@@ -1,1 +1,45 @@
-
+public class Main {
+	public static void main(String args[]){
+	      //get color factory
+	      AbstractFactory shapeFactory = FactoryProducer.getFactory("Shape");
+ 
+	      //Get Circle class object
+	      Shape shape1 = shapeFactory.getShape("Circle");
+ 
+	      //call drawShape method of Circle
+	      shape1.drawShape();
+ 
+	      //Get Rectangle class object
+	      Shape shape2 = shapeFactory.getShape("Rectangle");
+ 
+	      //call drawShape method of Rectangle
+	      shape2.drawShape();
+ 
+	      //Get Square class object
+	      Shape shape3 = shapeFactory.getShape("Square");
+ 
+	      //call drawShape method of Square
+	      shape3.drawShape();
+ 
+	      //get color factory
+	      AbstractFactory colorFactory = FactoryProducer.getFactory("Color");
+ 
+	      //get an object of Color Red
+	      Color color1 = colorFactory.getColor("Red");
+ 
+	      //call fillColor method of Red
+	      color1.fillColor();
+ 
+	      //get an object of Color Green
+	      Color color2 = colorFactory.getColor("Green");
+ 
+	      //call fillColor method of Green
+	      color2.fillColor();
+ 
+	      //get an object of Color Blue
+	      Color color3 = colorFactory.getColor("Blue");
+ 
+	      //call fillColor method of Color Blue
+	      color3.fillColor();
+	}
+}
