fa4d92755b8e55ad4da5d0795678414aa2cbaf6a
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2023-12-27 20:27:39 +0530

    Create ProductShipping.java

---
+++ Design Patterns/Structural Design Patterns/Facade Pattern/Subsystem/ProductShipping.java
@@ -0,0 +1,6 @@
+class ProductShipping {
+    public void shipProduct(int orderId) {
+        // Logic to ship the product
+        System.out.println("Shipping product for order: " + orderId);
+    }
+}
