f3ec11ed949b67aeb777af9a69a7c026730de5ab
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2023-12-27 20:27:04 +0530

    Create PaymentProcessing.java

---
+++ Design Patterns/Structural Design Patterns/Facade Pattern/Subsystem/PaymentProcessing.java
@@ -0,0 +1,6 @@
+class PaymentProcessing {
+    public void processPayment(int orderId) {
+        // Logic to process payment
+        System.out.println("Processing payment for order: " + orderId);
+    }
+}
