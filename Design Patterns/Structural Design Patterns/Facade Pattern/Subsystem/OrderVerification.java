51b814ec21adae60c5aabb67f72b5ae1041c104f
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2023-12-27 20:26:21 +0530

    Create OrderVerification.java

---
+++ Design Patterns/Structural Design Patterns/Facade Pattern/Subsystem/OrderVerification.java
@@ -0,0 +1,7 @@
+class OrderVerification {
+    public boolean verifyOrder(int orderId) {
+        // Logic to verify order
+        System.out.println("Verifying order: " + orderId);
+        return true;
+    }
+}
