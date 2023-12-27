f929be37d16650f61de0d57dd418b28a73c10af3
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2023-12-27 19:41:53 +0530

    Update Main.java

--- Design Patterns/Structural Design Patterns/Decorator Pattern/Client/Main.java
+++ Design Patterns/Structural Design Patterns/Decorator Pattern/Client/Main.java
@@ -1,1 +1,12 @@
+public class Main {
 
+	public static void main(String[] args) {
+		Car sportsCar = new SportsCar(new BasicCar());
+		sportsCar.assemble();
+		System.out.println("\n*****");
+		
+		Car sportsLuxuryCar = new SportsCar(new LuxuryCar(new BasicCar()));
+		sportsLuxuryCar.assemble();
+	}
+
+}
