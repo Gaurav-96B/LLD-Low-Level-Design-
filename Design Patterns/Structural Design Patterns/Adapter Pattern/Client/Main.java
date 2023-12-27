be47d4fb122be88c44bd78976d67e3844e7908f7
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2023-12-27 14:35:03 +0530

    Update Main.java

--- Design Patterns/Structural Design Patterns/Adapter Pattern/Client/Main.java
+++ Design Patterns/Structural Design Patterns/Adapter Pattern/Client/Main.java
@@ -1,1 +1,12 @@
+import LowLevelDesign.DesignPatterns.AdapterDesignPattern.Adaptee.WeightMachineForBabies;
+import LowLevelDesign.DesignPatterns.AdapterDesignPattern.Adapter.WeightMachineAdapter;
+import LowLevelDesign.DesignPatterns.AdapterDesignPattern.Adapter.WeightMachineAdapterImpl;
 
+public class Main {
+
+    public static void main(String args[]){
+
+        WeightMachineAdapter weightMachineAdapter = new WeightMachineAdapterImpl(new WeightMachineForBabies());
+        System.out.println(weightMachineAdapter.getWeightInKg());
+    }
+}
