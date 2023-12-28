2d0b220463e95490e583a4cd9e44a5546c56dac8
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2023-12-28 21:23:22 +0530

    Create ShoppingMall.java

---
+++ Solid Design Principles/DIP/Solution/ShoppingMall.java
@@ -0,0 +1,13 @@
+public class ShoppingMall {
+
+    private BankCard bankCard;
+
+    public ShoppingMall(BankCard bankCard) {
+        this.bankCard = bankCard;
+    }
+
+    public void doPurchaseSomething(long amount){
+        bankCard.doTransaction(amount);
+    }
+  
+}
