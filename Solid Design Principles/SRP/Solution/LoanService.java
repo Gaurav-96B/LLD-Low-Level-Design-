aaa780c000f5c3f87902783663f7c7295c12b513
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2023-12-28 20:21:23 +0530

    Create LoanService.java

---
+++ Solid Design Principles/SRP/Solution/LoanService.java
@@ -0,0 +1,14 @@
+public class LoanService {
+
+    public void getLoanInterestInfo(String loanType) {
+        if (loanType.equals("homeLoan")) {
+            //do some job
+        }
+        if (loanType.equals("personalLoan")) {
+            //do some job
+        }
+        if (loanType.equals("car")) {
+            //do some job
+        }
+    }
+}
