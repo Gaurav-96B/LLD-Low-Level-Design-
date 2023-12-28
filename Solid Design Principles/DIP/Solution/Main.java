51b1f34dba372dcaf064e38ce4c1719814735f1e
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2023-12-28 21:24:18 +0530

    Create Main.java

---
+++ Solid Design Principles/DIP/Solution/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+       // DebitCard debitCard=new DebitCard();
+       // CreditCard creditCard=new CreditCard();
+
+        BankCard bankCard=new CreditCard();
+        ShoppingMall shoppingMall=new ShoppingMall(bankCard);
+        shoppingMall.doPurchaseSomething(5000);
+    }
+}
