33c882aa3ca858635c837bd1e3433c8a31c9157e
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2023-12-28 20:59:25 +0530

    Create GooglePay.java

---
+++ Solid Design Principles/ISP/Problem/GooglePay.java
@@ -0,0 +1,14 @@
+public class GooglePay implements UPIPayments {
+
+    public void payMoney() {
+
+    }
+
+    public void getScratchCard() {
+
+    }
+
+    public void getCashBackAsCreditBalance() {
+      //this features is there in gpay
+    }
+}
