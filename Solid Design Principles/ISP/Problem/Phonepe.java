a488a864b9e653f99c53e00c6246736d5e44c961
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2023-12-28 21:03:58 +0530

    Update Phonepe.java

--- Solid Design Principles/ISP/Problem/Phonepe.java
+++ Solid Design Principles/ISP/Problem/Phonepe.java
@@ -1,10 +1,15 @@
 public class Phonepe implements UPIPayments {
+
     public void payMoney() {
 
     }
 
     public void getScratchCard() {
 
+    }
+    
+    public void getCashBackAsCreditBalance() {
+      //not applicable
     }
 
 
