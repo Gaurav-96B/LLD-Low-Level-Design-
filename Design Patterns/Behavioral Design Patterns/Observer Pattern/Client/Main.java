e51bb85590b32beef146fa07bf319e954498d202
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2024-01-02 17:19:26 +0530

    Update Main.java

--- Design Patterns/Behavioral Design Patterns/Observer Pattern/Client/Main.java
+++ Design Patterns/Behavioral Design Patterns/Observer Pattern/Client/Main.java
@@ -1,1 +1,17 @@
+public class Main{
+    public static void main(String[] args) {
+        // Create a concrete subject
+        ConcreteSubject subject = new ConcreteSubject();
 
+        // Create concrete observers
+        Observer observer1 = new ConcreteObserver("Observer 1");
+        Observer observer2 = new ConcreteObserver("Observer 2");
+
+        // Register observers with the subject
+        subject.registerObserver(observer1);
+        subject.registerObserver(observer2);
+
+        // Set the state of the subject
+        subject.setState("New state!");
+    }
+}
