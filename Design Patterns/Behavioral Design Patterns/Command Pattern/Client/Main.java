24b237e449d877abc962967942d38c7024866e9e
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2024-01-02 19:01:26 +0530

    Update Main.java

--- Design Patterns/Behavioral Design Patterns/Command Pattern/Client/Main.java
+++ Design Patterns/Behavioral Design Patterns/Command Pattern/Client/Main.java
@@ -1,1 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        // Create receiver
+        Light light = new Light();
 
+        // Create concrete commands
+        LightOnCommand lightOnCommand = new LightOnCommand(light);
+        LightOffCommand lightOffCommand = new LightOffCommand(light);
+
+        // Create invoker
+        RemoteControl remote = new RemoteControl();
+
+        // Set and press the button for turning on the light
+        remote.setCommand(lightOnCommand);
+        remote.pressButton();
+
+        // Set and press the button for turning off the light
+        remote.setCommand(lightOffCommand);
+        remote.pressButton();
+    }
+}
