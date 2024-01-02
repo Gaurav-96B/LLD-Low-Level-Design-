d2fdb54ec18408131eeff5b7bf908c75331d658c
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2024-01-02 17:23:58 +0530

    Create ConcreteObserver.java

---
+++ Design Patterns/Behavioral Design Patterns/Observer Pattern/Observer/ConcreteObserver.java
@@ -0,0 +1,13 @@
+// Concrete observer
+public class ConcreteObserver implements Observer {
+    private String name;
+
+    public ConcreteObserver(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void update(String message) {
+        System.out.println(name + " received an update: " + message);
+    }
+}
