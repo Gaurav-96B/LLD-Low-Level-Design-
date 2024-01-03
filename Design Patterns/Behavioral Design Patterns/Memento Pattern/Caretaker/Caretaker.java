24e85cbc3c7f6fc3a099e86e86a3b525c0ab09ee
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2024-01-03 13:23:29 +0530

    Create Caretaker.java

---
+++ Design Patterns/Behavioral Design Patterns/Memento Pattern/Caretaker/Caretaker.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Caretaker {
+    private final List<Memento> mementoList = new ArrayList<>();
+
+    public void addMemento(Memento state) {
+        mementoList.add(state);
+    }
+
+    public Memento getMemento(int index) {
+        return mementoList.get(index);
+    }
