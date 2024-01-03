6814d12aef017ec2394e8062ed81e53acc0c786e
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2024-01-03 14:29:11 +0530

    Update DebugBasedLogger.java

--- Design Patterns/Behavioral Design Patterns/Chain Of Responsibility Pattern/Handler/DebugBasedLogger.java
+++ Design Patterns/Behavioral Design Patterns/Chain Of Responsibility Pattern/Handler/DebugBasedLogger.java
@@ -11,8 +11,8 @@ public class DebugBasedLogger extends Logger {
         this.nextLevelLogger = nextLevelLogger;  
     }  
         
-    public void logMessage(String levels, String msg){  
-        if(this.levels<=levels){  
+    public void logMessage(Request request, String msg){  
+        if(this.levels<=request.getType()){  
             displayLogInfo(msg);  
         }  
         if (nextLevelLogger!=null) {  
