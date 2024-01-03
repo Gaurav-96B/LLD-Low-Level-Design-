d2ba3cd67f67786c8a512e79ba99ad56815b0e20
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2024-01-03 14:18:48 +0530

    Update Main.java

--- Design Patterns/Behavioral Design Patterns/Chain Of Responsibility Pattern/Client/Main.java
+++ Design Patterns/Behavioral Design Patterns/Chain Of Responsibility Pattern/Client/Main.java
@@ -1,1 +1,19 @@
-
+public class Client {
+    public static void main(String[] args) {
+         Logger chainLogger= doChaining();  
+         chainLogger.logMessage(request1, "Enter the sequence of values ");  
+         chainLogger.logMessage(request2, "An error is occured now");  
+         chainLogger.logMessage(request3, "This was the error now debugging is compeled"
+    }
+    private static Logger doChaining(){  
+          Logger consoleLogger = new ConsoleBasedLogger(Level.OUTPUTINFO);  
+            
+          Logger errorLogger = new ErrorBasedLogger(Level.ERRORINFO);  
+          consoleLogger.setNextLevelLogger(errorLogger);  
+            
+          Logger debugLogger = new DebugBasedLogger(Level.DEBUGINFO);  
+          errorLogger.setNextLevelLogger(debugLogger);  
+            
+          return consoleLogger;   
+          }  
+}
