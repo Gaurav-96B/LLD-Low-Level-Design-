dc2f743e704ce805d19af6d0099bc5d88a3c93b9
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2024-01-03 12:35:04 +0530

    Update Main.java

--- Design Patterns/Behavioral Design Patterns/Mediator Pattern/Client/Main.java
+++ Design Patterns/Behavioral Design Patterns/Mediator Pattern/Client/Main.java
@@ -1,1 +1,15 @@
-
+public class Main {
+    public static void main(String[] args) {
+        Mediator mediator = new ConcreteMediator();
+        Colleague colleague1 = new ConcreteColleague(mediator, "Pankaj");
+        Colleague colleague2 = new ConcreteColleague(mediator, "Lisa");
+        Colleague colleague3 = new ConcreteColleague(mediator, "Saurabh");
+        Colleague colleague4 = new ConcreteColleague(mediator, "David");
+        mediator.addUser(colleague1);
+		    mediator.addUser(colleague2);
+		    mediator.addUser(colleague3);
+		    mediator.addUser(colleague4);
+        colleague1.sendMessage("Hello, colleague2!");
+        colleague2.sendMessage("Hi, colleague1!");
+    }
+}
