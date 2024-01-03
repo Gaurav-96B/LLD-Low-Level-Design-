1588a9a5db01c1c1eae8a8c9c73c281a92a21443
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2024-01-03 14:33:12 +0530

    Update Logger.java

--- Design Patterns/Behavioral Design Patterns/Chain Of Responsibility Pattern/Handler/Logger.java
+++ Design Patterns/Behavioral Design Patterns/Chain Of Responsibility Pattern/Handler/Logger.java
@@ -1,5 +1,5 @@
 public interface Handler {
     void setNextHandler(Handler nextHandler);
-    void logMessage(Request request);
+    void logMessage(Request request,String msg);
     void displayLogInfo(String msg);
 }
