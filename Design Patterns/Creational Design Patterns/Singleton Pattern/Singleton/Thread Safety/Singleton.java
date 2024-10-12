1a1cf98c9896b2bccbfd3d707e6c33130e5ac54c
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2024-10-12 23:22:14 +0530

    Update Singleton.java

--- Design Patterns/Creational Design Patterns/Singleton Pattern/Singleton/Thread Safety/Singleton.java
+++ Design Patterns/Creational Design Patterns/Singleton Pattern/Singleton/Thread Safety/Singleton.java
@@ -5,6 +5,15 @@ public class Singleton {
         // Initialization code
     }
 
+    public synchronized static Singleton getInstance() {
+        if (instance == null) {
+             instance = new Singleton();
+            }
+        return instance;
+    }
+
+             OR
+    
     // Double-checked locking for thread safety
     public static Singleton getInstance() {
         if (instance == null) {
