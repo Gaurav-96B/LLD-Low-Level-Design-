aad2040b7578fff2d66a803f5f65a53764973859
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2024-10-12 23:47:43 +0530

    Update Main.java

--- Design Patterns/Creational Design Patterns/Factory Method Pattern/Client/Main.java
+++ Design Patterns/Creational Design Patterns/Factory Method Pattern/Client/Main.java
@@ -9,13 +9,13 @@ public class Main {
       shape1.draw();
 
       //get an object of Rectangle and call its draw method.
-      Shape shape2 = shapeFactory.getShape("RECTANGLE");
+      Shape shape2 = ShapeFactory.getShape("RECTANGLE");
 
       //call draw method of Rectangle
       shape2.draw();
 
       //get an object of Square and call its draw method.
-      Shape shape3 = shapeFactory.getShape("SQUARE");
+      Shape shape3 = ShapeFactory.getShape("SQUARE");
 
       //call draw method of square
       shape3.draw();
