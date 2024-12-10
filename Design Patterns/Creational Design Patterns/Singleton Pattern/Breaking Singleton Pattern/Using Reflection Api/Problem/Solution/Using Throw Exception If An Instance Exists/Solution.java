c2b5a6f2ead30e5e75bb0a11f47a87f39bc2ff83
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2024-12-10 20:15:30 +0530

    Update Solution.java

--- Design Patterns/Creational Design Patterns/Singleton Pattern/Breaking Singleton Pattern/Using Reflection Api/Problem/Solution/Using Throw Exception If An Instance Exists/Solution.java
+++ Design Patterns/Creational Design Patterns/Singleton Pattern/Breaking Singleton Pattern/Using Reflection Api/Problem/Solution/Using Throw Exception If An Instance Exists/Solution.java
@@ -1,1 +1,20 @@
+public class Singleton {
+    private static Singleton instance;
 
+    private Singleton() {
+        if (instance != null) {
+            throw new RuntimeException("Reflection is not allowed!");
+        }
+    }
+
+    public static Singleton getInstance() {
+        if (instance == null) {
+            synchronized (Singleton.class) {
+                if (instance == null) {
+                    instance = new Singleton();
+                }
+            }
+        }
+        return instance;
+    }
+}
