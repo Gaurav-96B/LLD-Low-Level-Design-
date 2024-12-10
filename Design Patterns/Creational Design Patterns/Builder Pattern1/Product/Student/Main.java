0ff0dbc30184b7a961016c301f33608ce222c013
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2024-12-10 19:08:31 +0530

    Update Main.java

--- Design Patterns/Creational Design Patterns/Builder Pattern1/Product/Student/Main.java
+++ Design Patterns/Creational Design Patterns/Builder Pattern1/Product/Student/Main.java
@@ -1,39 +1,37 @@
 class Student{
-    String name;
-    int roll_no;
+    private int id;
+    private String name;
     
-    private Student(StudentBuilder studentBuilder){
+    public Student(StudentBuilder studentBuilder){
+        this.id=studentBuilder.id;
         this.name=studentBuilder.name;
-        this.roll_no=studentBuilder.roll_no;
     }
     
     public String toString(){
-        return this.name+" "+this.roll_no;
+        return this.id+" "+this.name;
     }
     
     public static class StudentBuilder{
-        String name;
-        int roll_no;
+        private int id;
+        private String name;
         
-        StudentBuilder setName(String name){
+        public StudentBuilder setId(int id){
+            this.id=id;
+            return this;
+        }
+        public StudentBuilder setName(String name){
             this.name=name;
             return this;
         }
-        StudentBuilder setRollNo(int roll_no){
-            this.roll_no=roll_no;
-            return this;
-        }
-        
         public Student build(){
             return new Student(this);
         }
     }
 }
+
 class Main {
-    public static void main(String[] args) throws CloneNotSupportedException {
-   Student student=new Student.StudentBuilder()
-   .setName("saurav")
-   .setRollNo(123).build();
-   System.out.println(student);
+    public static void main(String[] args){
+        Student student=new Student.StudentBuilder().setId(1).setName("Saurav").build();
+        System.out.println(student);
     }
 }
