1d7fe47b8c2045e5a47afb58fbb5d11bb73f3382
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2024-12-10 19:11:17 +0530

    Update Main.java

--- Design Patterns/Creational Design Patterns/Builder Pattern1/Product/StudentBuilder/Main.java
+++ Design Patterns/Creational Design Patterns/Builder Pattern1/Product/StudentBuilder/Main.java
@@ -1,1 +1,37 @@
+class Student{
+    private int id;
+    private String name;
+    
+    public Student(StudentBuilder studentBuilder){
+        this.id=studentBuilder.id;
+        this.name=studentBuilder.name;
+    }
+    
+    public String toString(){
+        return this.id+" "+this.name;
+    }
+    
+    public static class StudentBuilder{
+        private int id;
+        private String name;
+        
+        public StudentBuilder setId(int id){
+            this.id=id;
+            return this;
+        }
+        public StudentBuilder setName(String name){
+            this.name=name;
+            return this;
+        }
+        public Student build(){
+            return new Student(this);
+        }
+    }
+}
 
+class Main {
+    public static void main(String[] args){
+        Student student=new Student.StudentBuilder().setId(1).setName("Saurav").build();
+        System.out.println(student);
+    }
+}
