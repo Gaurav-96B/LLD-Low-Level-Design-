daee38d56b98e8a8b52d8af2e9aa2e9eb9636bec
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2025-01-23 11:11:58 +0530

    Create PayPalPayment.java

---
+++ Design Patterns/Behavioral Design Patterns/Strategy Pattern/Shopping Cart System/Strategy/PayPalPayment.java
@@ -0,0 +1,10 @@
+class PayPalPayment implements PaymentStrategy{
+    String email;
+    
+    public PayPalPayment(String email){
+        this.email=email;
+    }
+    public void pay(double amount) {
+        System.out.println("Paid $" + amount + " using PayPal: " + email);
+    }
+}
