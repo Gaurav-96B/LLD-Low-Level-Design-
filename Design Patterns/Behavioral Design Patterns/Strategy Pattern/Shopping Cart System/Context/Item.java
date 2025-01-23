1dca8ebee6628d9e5d12b8f9acb6f8594362a94f
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2025-01-23 11:07:24 +0530

    Create Item.java

---
+++ Design Patterns/Behavioral Design Patterns/Strategy Pattern/Shopping Cart System/Context/Item.java
@@ -0,0 +1,12 @@
+class Item{
+    String itemName;
+    double price;
+    
+    public Item(String itemName,double price){
+        this.itemName=itemName;
+        this.price=price;
+    }
+    public double getPrice(){
+        return price;
+    } 
+}
