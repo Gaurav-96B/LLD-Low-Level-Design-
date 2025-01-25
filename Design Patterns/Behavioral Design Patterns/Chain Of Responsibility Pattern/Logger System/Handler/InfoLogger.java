63de2066906eb994cb01e7879a87c1a56f1f54b4
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2025-01-25 16:08:03 +0530

    Create InfoLogger.java

---
+++ Design Patterns/Behavioral Design Patterns/Chain Of Responsibility Pattern/Logger System/Handler/InfoLogger.java
@@ -0,0 +1,18 @@
+class InfoLogger implements Logger {
+    private Logger nextLogger;
+
+    @Override
+    public void setNextLogger(Logger nextLogger) {
+        this.nextLogger = nextLogger;
+    }
+
+    @Override
+    public void logMessage(int level, String message) {
+        if (level == INFO) {
+            System.out.println("INFO: " + message);
+        }
+        if (nextLogger != null) {
+            nextLogger.logMessage(level, message);
+        }
+    }
+}
