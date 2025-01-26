efa656a55f2a9bd200e09575befef284271de2e8
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2025-01-26 11:10:31 +0530

    Create TextFile.java

---
+++ Design Patterns/Behavioral Design Patterns/Visitor Design Pattern/File System Operations/Element/TextFile.java
@@ -0,0 +1,16 @@
+class TextFile implements File {
+    private String name;
+
+    public TextFile(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public void accept(Visitor visitor) {
+        visitor.visit(this);
+    }
+}
