abefe33c0fecb7634b6735d832ba78808e965070
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2025-01-26 11:12:26 +0530

    Update Main.java

--- Design Patterns/Behavioral Design Patterns/Visitor Design Pattern/File System Operations/Client/Main.java
+++ Design Patterns/Behavioral Design Patterns/Visitor Design Pattern/File System Operations/Client/Main.java
@@ -1,1 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+        // Object structure
+        List<File> files = new ArrayList<>();
+        files.add(new TextFile("document.txt"));
+        files.add(new ImageFile("photo.jpg"));
 
+        // Visitors
+        Visitor virusScanner = new VirusScanner();
+        Visitor compressor = new Compressor();
+
+        // Process files with virus scanner
+        System.out.println("Running virus scan...");
+        for (File file : files) {
+            file.accept(virusScanner);
+        }
+
+        // Process files with compressor
+        System.out.println("\nRunning compression...");
+        for (File file : files) {
+            file.accept(compressor);
+        }
+    }
+}
