e382bffd3e9ec7bb0f39cb2a8a6e22264496a221
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2025-01-26 11:10:56 +0530

    Create ImageFile.java

---
+++ Design Patterns/Behavioral Design Patterns/Visitor Design Pattern/File System Operations/Element/ImageFile.java
@@ -0,0 +1,16 @@
+class ImageFile implements File {
+    private String name;
+
+    public ImageFile(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public void accept(Visitor visitor) {
+        visitor.visit(this);
+    }
+}
