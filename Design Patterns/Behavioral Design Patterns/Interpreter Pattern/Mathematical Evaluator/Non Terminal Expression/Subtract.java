ec7cfe21ed6ffae520ff24c082271bbb53530d3c
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2025-01-27 14:11:46 +0530

    Create Subtract.java

---
+++ Design Patterns/Behavioral Design Patterns/Interpreter Pattern/Mathematical Evaluator/Non Terminal Expression/Subtract.java
@@ -0,0 +1,14 @@
+class Subtract implements Expression {
+    private final Expression left;
+    private final Expression right;
+
+    public Subtract(Expression left, Expression right) {
+        this.left = left;
+        this.right = right;
+    }
+
+    @Override
+    public int interpret() {
+        return left.interpret() - right.interpret();
+    }
+}
