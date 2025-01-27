21ceda498cbb1a749402b5dc1740dde9ba9749a8
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2025-01-27 14:10:36 +0530

    Create Add.java

---
+++ Design Patterns/Behavioral Design Patterns/Interpreter Pattern/Mathematical Evaluator/Non Terminal Expression/Add.java
@@ -0,0 +1,14 @@
+class Add implements Expression {
+    private final Expression left;
+    private final Expression right;
+
+    public Add(Expression left, Expression right) {
+        this.left = left;
+        this.right = right;
+    }
+
+    @Override
+    public int interpret() {
+        return left.interpret() + right.interpret();
+    }
+}
