3e96ca7cea12a1be16c1c8b2b89f4c3978e34227
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2025-09-06 12:05:34 +0530

    Update Subject.java

--- Design Patterns/Behavioral Design Patterns/Observer Pattern/Stock Market Monitoring System/Subject/Subject.java
+++ Design Patterns/Behavioral Design Patterns/Observer Pattern/Stock Market Monitoring System/Subject/Subject.java
@@ -1,5 +1,5 @@
-interface Subject {
-    void registerObserver(Observer observer);
-    void unregisterObserver(Observer observer);
-    void notifyObservers();
+interface Subject{
+    public void addObserver(Observer observer);
+    public void remmoveObserver(Observer observer);
+    public void notifyObserver();
 }
