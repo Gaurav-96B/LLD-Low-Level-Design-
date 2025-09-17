14bf68bff6378dad0828dc67bafb580faf018cda
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2025-09-17 17:31:55 +0530

    Update Stock.java

--- Design Patterns/Behavioral Design Patterns/Observer Pattern/Stock Market Monitoring System/Subject/Stock.java
+++ Design Patterns/Behavioral Design Patterns/Observer Pattern/Stock Market Monitoring System/Subject/Stock.java
@@ -1,7 +1,7 @@
-class Stock{
+class Stock implements Subject{
     private String stockName;
     private double stockPrice;
-    List<Observer>observers;
+    private List<Observer>observers;
     public Stock(String stockName,double stockPrice){
         this.stockName=stockName;
         this.stockPrice=stockPrice;
@@ -14,11 +14,13 @@ class Stock{
     public void removeObserver(Observer observer){
         observers.remove(observer);
     }
+    
     public void notifyObserver(){
         for(Observer observer:observers){
             observer.update(stockName,stockPrice);
         }
     }
+    
     public void setPrice(double stockPrice){
         this.stockPrice=stockPrice;
         notifyObserver();
