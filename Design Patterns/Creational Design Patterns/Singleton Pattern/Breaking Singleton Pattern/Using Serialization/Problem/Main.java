af3988dbba5e08d967503f6c155bf3a1d8820629
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2026-06-12 19:42:53 +0530

    Update Main.java

--- Design Patterns/Creational Design Patterns/Singleton Pattern/Breaking Singleton Pattern/Using Serialization/Problem/Main.java
+++ Design Patterns/Creational Design Patterns/Singleton Pattern/Breaking Singleton Pattern/Using Serialization/Problem/Main.java
@@ -1,1 +1,23 @@
+import java.io.Serializable;
+class Singleton implements Serializable {
+   private static final Singleton instance =new Singleton();
 
+    private Singleton() {
+    }
+
+    public static Singleton getInstance() {
+        return instance;
+    }
+}
+class Main{
+  public static void main(String args[]){
+    Singleton s1 = Singleton.getInstance();
+    ObjectOutputStream oos =new ObjectOutputStream(new FileOutputStream("file.txt"));
+    oos.writeObject(s1);
+    oos.close();
+    ObjectInputStream ois =new ObjectInputStream(new FileInputStream("file.txt"));
+    Singleton s2 =(Singleton) ois.readObject();
+    System.out.println(s1.hashCode());
+    System.out.println(s2.hashCode());
+}
+}
