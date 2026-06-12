4f8bd60e4fe19c7b3c20c9a61cc839350c4af056
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2026-06-12 19:51:22 +0530

    Update Singleton.java

--- Design Patterns/Creational Design Patterns/Singleton Pattern/Singleton/Singleton using Bill Pugh’s Inner Static Class/Singleton.java
+++ Design Patterns/Creational Design Patterns/Singleton Pattern/Singleton/Singleton using Bill Pugh’s Inner Static Class/Singleton.java
@@ -8,14 +8,5 @@ class Singleton {
     public static Singleton getInstance() {
         return SingletonHelper.INSTANCE;
     }
-}
 
-class Main {
-    public static void main(String[] args) {
-        Singleton instance1 = Singleton.getInstance();
-        Singleton instance2 = Singleton.getInstance();
-
-        // Both instances refer to the same object
-        System.out.println(instance1 == instance2); // Output: true
-    }
 }
